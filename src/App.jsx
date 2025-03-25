03d11d28e62cca04e1f3940d69911f7f74d686ba
John Rothera <johnwrothera@gmail.com>
2025-03-25 16:32:54 +0000

    Made a gig component, added a bunch of css to make it look half decent.

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,43 @@
-import Hello from "./components/Hello";
-import makersLogo from "./assets/Makers-Logo.png";
+import Gig from "./components/Gig";
+import convergeImage from "./assets/Converge_Observatory-20.png";
+import charliImage from "./assets/charliexcx.png";
 import "./App.css";
 
 function App() {
   return (
-    <>
-      <Hello name="World" />
-      <img className="logo" src={makersLogo}></img>
-    </>
+    <div className="gigs-container">
+      <h1 className="app-title">Upcoming Gigs</h1>
+      <div className="gigs-grid">
+        <Gig 
+          name="Converge"
+          image={convergeImage}
+          description="Converge are back! Playing Electric Brixton with support from IHateGod"
+          date="27th April 2025"
+          location="London - Brixton"
+        />
+        <Gig
+          name="Charli XCX"
+          image={charliImage}
+          description="Finishing up her world tour at Wembley Arena. Time for another Brat Summer!!"
+          date="23rd August 2025"
+          location="London - Wembley Arena"
+        />
+        <Gig
+          name="Charli XCX"
+          image={charliImage}
+          description="Finishing up her world tour at Wembley Arena. Time for another Brat Summer!!"
+          date="23rd August 2025"
+          location="London - Wembley Arena"
+        />
+        <Gig
+          name="Charli XCX"
+          image={charliImage}
+          description="Finishing up her world tour at Wembley Arena. Time for another Brat Summer!!"
+          date="23rd August 2025"
+          location="London - Wembley Arena"
+        />
+      </div>
+    </div>
   );
 }
 
---
+++ src/components/Gig.jsx
@@ -0,0 +1,15 @@
+import "./Gig.css"
+
+const Gig = (props) => {
+    return (
+        <div className="gig-card">
+            <h3 className="bandname">{props.name}</h3>
+            <img className="gigImage" src={props.image} alt={props.name + " live"}></img>
+            <p className="gigDescription">{props.description}</p>
+            <p className="gigDate">{props.date}</p>
+            <p className="gigLocation">{props.location}</p>
+        </div>
+    )
+};
+
+export default Gig;
