ea1df1244324a7bc7a9df2e15c8b43858437a2a1
John Rothera <johnwrothera@gmail.com>
2025-03-26 15:48:46 +0000

    added more Gig containers and added test for Gig component.

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import Gig from "./components/Gig";
 import convergeImage from "./assets/Converge_Observatory-20.png";
 import charliImage from "./assets/charliexcx.png";
+import turnstileImage from "./assets/Turnstile-7.png";
+import dinosaurJrImage from "./assets/Dinosaur_Jr.png";
 import "./App.css";
 
 function App() {
@@ -23,18 +25,18 @@ function App() {
           location="London - Wembley Arena"
         />
         <Gig
-          name="Charli XCX"
-          image={charliImage}
-          description="Finishing up her world tour at Wembley Arena. Time for another Brat Summer!!"
-          date="23rd August 2025"
-          location="London - Wembley Arena"
+          name="Turnstile"
+          image={turnstileImage}
+          description="Turnstile are heading back to the UK for Glastonbury Festival!"
+          date="27th June 2025"
+          location="Glastonbury Farm"
         />
         <Gig
-          name="Charli XCX"
-          image={charliImage}
-          description="Finishing up her world tour at Wembley Arena. Time for another Brat Summer!!"
-          date="23rd August 2025"
-          location="London - Wembley Arena"
+          name="Dinosaur Jr"
+          image={dinosaurJrImage}
+          description="J Mascis and the good buds are back out on a worldwide tour"
+          date="15th November 2025"
+          location="Nottingham - Rescue Rooms"
         />
       </div>
     </div>
---
+++ tests/Gig.test.jsx
@@ -0,0 +1,35 @@
+import { screen, render } from "@testing-library/react";
+
+import Gig from "../src/components/Gig";
+
+test("Render page with all elements", () => {
+  const { container } = render(
+    <Gig
+      name="Megadeth"
+      image="https://cdn.britannica.com/08/252308-050-D0C3221F/Members-of-thrash-metal-band-Megadeth-1986.jpg"
+      description="Reunited with Marty Friedman to play Rust In Peace in full!"
+      date="2026"
+      location="Rock City - Nottingham"
+    />
+  );
+
+  expect(screen.getByRole("heading", { level: 3 })).toHaveTextContent(
+    "Megadeth"
+  );
+
+  const image = screen.getByRole("img");
+  expect(image).toHaveTextContent("");
+  expect(image).toHaveClass("gigImage");
+
+  const paragraph = container.querySelectorAll("p");
+  expect(paragraph[0]).toHaveTextContent(
+    "Reunited with Marty Friedman to play Rust In Peace in full!"
+  );
+  expect(paragraph[0]).toHaveClass("gigDescription");
+
+  expect(paragraph[1]).toHaveTextContent("2026");
+  expect(paragraph[1]).toHaveClass("gigDate");
+
+  expect(paragraph[2]).toHaveTextContent("Rock City - Nottingham");
+  expect(paragraph[2]).toHaveClass("gigLocation");
+});
