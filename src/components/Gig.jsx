bcecfc908b14e04e5f6e143f415d649185359fef
John Rothera <johnwrothera@gmail.com>
2025-03-27 13:55:34 +0000

    Created a favourite button and state handler. Updated the tests to monitor state.

--- src/components/Gig.jsx
+++ src/components/Gig.jsx
@@ -1,8 +1,22 @@
+import { useState } from "react";
 import "./Gig.css";
 
 const Gig = (props) => {
+  const [fave, setFave] = useState("😈");
+
+  const setFavourite = () => {
+    if (fave === "😈") {
+      setFave("👍");
+      alert("You're right, this show's gonna be killer!!");
+    } else if (fave === "👍") {
+      setFave("😈");
+      alert("Bummer man! This was gonna rock!");
+    }
+  };
+
   return (
     <div className="gig-card">
+      {fave === "👍" && <h4 className="favourited">Favourited!</h4>}
       <h3 className="bandname">{props.name}</h3>
       <img
         className="gigImage"
@@ -12,6 +26,9 @@ const Gig = (props) => {
       <p className="gigDescription">{props.description}</p>
       <p className="gigDate">{props.date}</p>
       <p className="gigLocation">{props.location}</p>
+      <button className="favouriteButton" onClick={setFavourite}>
+        {fave}
+      </button>
     </div>
   );
 };
--- tests/Gig.test.jsx
+++ tests/Gig.test.jsx
@@ -1,5 +1,5 @@
 import { screen, render } from "@testing-library/react";
-
+import userEvent from "@testing-library/user-event";
 import Gig from "../src/components/Gig";
 
 test("Render page with all elements", () => {
@@ -13,6 +13,42 @@ test("Render page with all elements", () => {
     />
   );
 
+  expect(screen.getByRole("heading", { level: 3 })).toHaveTextContent(
+    "Megadeth"
+  );
+
+  const image = screen.getByRole("img");
+  expect(image).toHaveTextContent("");
+  expect(image).toHaveClass("gigImage");
+
+  const paragraph = container.querySelectorAll("p");
+  expect(paragraph[0]).toHaveTextContent(
+    "Reunited with Marty Friedman to play Rust In Peace in full!"
+  );
+  expect(paragraph[0]).toHaveClass("gigDescription");
+
+  expect(paragraph[1]).toHaveTextContent("2026");
+  expect(paragraph[1]).toHaveClass("gigDate");
+
+  expect(paragraph[2]).toHaveTextContent("Rock City - Nottingham");
+  expect(paragraph[2]).toHaveClass("gigLocation");
+});
+
+test("Test for favourited gig state", async () => {
+  const { container } = render(
+    <Gig
+      name="Megadeth"
+      image="https://cdn.britannica.com/08/252308-050-D0C3221F/Members-of-thrash-metal-band-Megadeth-1986.jpg"
+      description="Reunited with Marty Friedman to play Rust In Peace in full!"
+      date="2026"
+      location="Rock City - Nottingham"
+    />
+  );
+
+  await userEvent.click(screen.getByText("😈"));
+  expect(screen.getByRole("heading", { level: 4 })).toHaveTextContent(
+    "Favourited!"
+  );
   expect(screen.getByRole("heading", { level: 3 })).toHaveTextContent(
     "Megadeth"
   );
